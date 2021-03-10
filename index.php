0b279f43653752e7f91fa5e7083499c75e6f2c7f
Viruzzz <mvt3am@yahoo.com>
2021-03-10 22:39:08 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Blockade - OktoDark</title>
+		<meta name="keywords" content="" />
+		<meta name="description" content="" />
+		<link href="http://fonts.googleapis.com/css?family=Varela" rel="stylesheet" />
+		<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+		<!--[if lt IE 9]>
+			<script src="https://cdn.oktodark.com/game/blockade/js/html5shiv.js"></script>
+			<script src="https://cdn.oktodark.com/game/blockade/js/respond.min.js"></script>
+		<![endif]-->
+		<link rel="shortcut icon" href="https://cdn.oktodark.com/game/blockade/images/favicon.ico">
+		<meta name="google-site-verification" content="bKPt9IGPvVVP2QcBkKjv9jnmHPhqlhY59lRzxezPjEY" />
+	</head>
+	<body>
+		<div id="wrapper">
+			<div id="header-wrapper">
+				<div id="header" class="container">
+					<div id="logo">
+						<a href="#top"><img src="https://cdn.oktodark.com/game/blockade/images/logo.png" alt="logo"/></a>
+					</div>
+					<div id="menu">
+						<ul>
+							<li><a href="#top" title="">Homepage</a></li>
+							<li><a href="#about" title="">About</a></li>
+							<li><a href="#requirements" title="">Requirements</a></li>
+							<li><a href="#roadmap" title="">Roadmap</a></li>
+							<li><a href="https://www.oktodark.com" title="">Main Website</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+			<a name="about"></a>
+			<div id="page" class="container">
+				<div class="title">
+					<h2>About</h2>
+				</div>
+				<p>Blockade is our first game and is in the development stage. New and updated versions of the game will be released every few weeks. The game is easy, just find your way to the exit of the maze.</p>
+				<div class="w3-content w3-display-container">
+					<h4>Screenshots</h4>
+					<img src="images/image1.png" class="ingame" alt="" />
+					<img src="images/image2.png" class="ingame" alt="" />
+					<img src="images/image3.png" class="ingame" alt="" />
+					<img src="images/image4.png" class="ingame" alt="" />
+					<img src="images/image5.png" class="ingame" alt="" />
+					<button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
+					<button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
+				</div>
+			</div>
+
+			<div id="banner">
+				<div class="container">
+					<div class="title">
+						<h2>Download</h2>
+					</div>
+					<ul class="actions">
+						<li><b>Latest Releases v1.2.1</b></li>
+						<li><br></li>
+						<li><a href="https://www.oktodark.com/download/now.php?id=Blockade.v1.2.1.win" class="bwin"><i class="fab fa-windows" aria-hidden="true"></i> Windows</a></li>
+					</ul>
+					<span class="note">The game is archived and requires unzipping. Use the 7zip program, it's free.</span>
+				</div>
+			</div>
+
+			<a name="requirements"></a>
+			<div id="featured">
+				<div class="container">
+					<div class="title">
+						<h2>System Requirements</h2>
+						<br>
+						<div class="lefttext">
+							<h3>Windows</h3>
+							<br>
+							OS: 32 or 64bit of Windows 7, 8 or 10<br>
+							DirectX: Version 11<br>
+							Processor: Intel Core i5 or AMD equivalent<br>
+							Memory: 2GB RAM<br>
+							Graphics: Nvidia GTX 650 TI or AMD equivalent<br>
+							Storage: 2GB Free Space
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<a name="roadmap"></a>
+			<div id="extra" class="container">
+				<div class="title">
+					<h2>Roadmap</h2>
+					<div class="note">
+						<span class="byline">Please note that the game is in Alpha version.<br>Below you will see the functions we hope to add in the near future.<br>Trello Board: <a href="https://trello.com/b/JkrfupIg/blockade">https://trello.com/b/JkrfupIg/blockade</a></span>
+					</div>
+				</div>
+
+				<div id="three-column">
+					<div class="boxA">
+						<div class="box">
+							<i class="fa fa-save"></i>
+							<h3>Save & Load</h3>
+							<p>An improvement to this function is being worked on at the moment. (!)</p>
+						</div>
+					</div>
+					<div class="boxB">
+						<div class="box">
+							<i class="fa fa-umbrella"></i>
+							<h3>MainMenu</h3>
+							<p>A new Main menu will be available within a few days.</p>
+						</div>
+					</div>
+					<div class="boxC">
+						<div class="box">
+							<i class="fa fa-music"></i>
+							<h3>Music</h3>
+							<p>Currently we are in negotiation for the rights to have in game music.</p>
+						</div>
+					</div>
+				</div>
+
+				<div id="three-column">
+					<div class="boxA">
+						<div class="box">
+							<i class="fa fa-desktop"></i>
+							<h3>In Game</h3>
+							<p>Currently there is no in game help. We are considering the addition of a compass to help players.</p>
+						</div>
+					</div>
+					<div class="boxB">
+						<div class="box">
+							<i class="fa fa-male"></i>
+							<h3>Character</h3>
+							<p>The game does not have a character, you play in the 'First Person View'.</p>
+						</div>
+					</div>
+					<div class="boxC">
+						<div class="box">
+							<i class="fa fa-gift"></i>
+							<h3>Your Choice</h3>
+							<p>All ideas for improvements to the game will be most welcome. Please use the <a href="https://github.com/OktoDark/Blockade/issues">bugs</a> website.</p>
+						</div>
+					</div>
+				</div>
+
+				<ul class="actions">
+					<li><a href="https://forum.oktodark.com/viewtopic.php?f=4&t=7" class="changelog">Changelog</a></li>
+				</ul>
+			</div>
+	
+
+	
+		</div>
+		<div id="copyright" class="container">
+			<p>2021 &copy; OktoDark. All rights reserved.</p>
+		</div>
+        <script>
+        var slideIndex = 1;
+        showDivs(slideIndex);
+
+        function plusDivs(n) {
+          showDivs(slideIndex += n);
+        }
+
+        function showDivs(n) {
+          var i;
+          var x = document.getElementsByClassName("ingame");
+          if (n > x.length) {slideIndex = 1}
+          if (n < 1) {slideIndex = x.length}
+          for (i = 0; i < x.length; i++) {
+             x[i].style.display = "none";
+          }
+          x[slideIndex-1].style.display = "block";
+        }
+        </script>
+	</body>
+</html>
